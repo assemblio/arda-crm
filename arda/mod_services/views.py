15aa29655080990b9fd70b27c367ca57b4352526
endrit-b <endriti.92@gmail.com>
2015-03-14 20:15:33 +0100

    provided services for a company impemented

--- arda/mod_services/views.py
+++ arda/mod_services/views.py
@@ -15,12 +15,13 @@ def services():
 @mod_services.route('/<string:company_name>', methods=['GET'])
 def company_services(company_name):
 
-    query = {
-        'company.slug': company_name
-    }
-    customer = mongo.db.customers.find_one(query)
+    services = get_services_for_given_company(company_name)
 
-    return render_template('mod_services/services.html', result=customer)
+    return render_template(
+        'mod_services/services.html',
+        company_name=company_name,
+        result_services=services
+    )
 
 
 @mod_services.route('/<string:company_name>/<string:customer_id>', methods=['GET'])
@@ -32,17 +33,21 @@ def customer_services(company_name, customer_id):
     }
 
     customer = mongo.db.customers.find_one(query)
-    print customer
 
     return render_template(
         'mod_services/services.html',
+        company_name=company_name,
+        customer_id=customer_id,
         result=customer
     )
 
 
-@mod_services.route('/edit/<string:company_name>/<string:customer_id>', methods=['POST'])
-def edit_service(company_name, customer_id):
+@mod_services.route('/edit', methods=['POST'])
+def edit_service():
 
+    company_name = request.form['company_name']
+    customer_id = request.form['customer_id']
+    #services
     provided_service = request.form['providedService']
     date = request.form['date']
     description = request.form['description']
@@ -64,3 +69,59 @@ def edit_service(company_name, customer_id):
         }
     )
     return redirect(url_for('services.customer_services', company_name=company_name, customer_id=customer_id))
+
+
+def get_services_for_given_company(company_name):
+
+    json_obj = mongo.db.customers.aggregate([
+        {
+            "$match": {
+                "company.slug": company_name
+            }
+        },
+        {
+            "$unwind": "$provided_services"
+        },
+        {
+            "$group": {
+                "_id": {
+                    "company": {
+                        "name": "$company.name",
+                        "slug": "$company.slug",
+                    },
+                    "customer": {
+                        "firstName": "$first_name",
+                        "lastName": "$last_name",
+                        "customerId": "$_id"
+                    },
+                    "service": {
+                        "type": "$provided_services.provided_service",
+                        "description": "$provided_services.description",
+                        "fee": "$provided_services.service_fee",
+                        "date": "$provided_services.service_date"
+                    }
+                }
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "company": {
+                    "name": "$_id.company.name",
+                    "slug": "$_id.company.slug",
+                },
+                "customer": {
+                    "firstName": "$_id.customer.firstName",
+                    "lastName": "$_id.customer.lastName",
+                    "customerId": "$_id.customer.customerId",
+                },
+                "service": {
+                    "type": "$_id.service.type",
+                    "description": "$_id.service.description",
+                    "fee": "$_id.service.fee",
+                    "date": "$_id.service.date"
+                }
+            }
+        }
+    ])
+    return json_obj['result']
