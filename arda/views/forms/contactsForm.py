973a4d6c9490f596889299247ad049f16a2c5728
endrit-b <endriti.92@gmail.com>
2015-03-18 10:55:39 +0100

    removed views folder

--- arda/views/forms/contactsForm.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import Form
-from wtforms import SelectField, TextField
-
-class ContactsForm(Form):
-
-    job_type = SelectField("Job type", choices=[('Municipality', 'Municipality')])
