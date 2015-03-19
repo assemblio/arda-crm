e84ddbc0343d3c97100b6c19c14ae3759e291990
endrit-b <endriti.92@gmail.com>
2015-03-19 21:36:45 +0100

    search feature added, but yet to fix the operations buttons urls

--- arda/mod_api/views.py
+++ arda/mod_api/views.py
@@ -3,6 +3,8 @@ from flask.ext.security import login_required
 from arda import mongo
 from bson import json_util
 from datetime import datetime
+from slugify import slugify
+
 mod_api = Blueprint('api', __name__, url_prefix='/api')
 
 
@@ -57,6 +59,77 @@ def date_fee_chart():
 
     json_obj = mongo.db.customers.aggregate(pipeline)
 
+    resp = Response(
+        response=json_util.dumps(json_obj['result']),
+        mimetype='application/json'
+    )
+
+    return resp
+
+
+@mod_api.route('/search/customers', methods=['GET'])
+@login_required
+def search():
+    if(len(request.args) > 0):
+        f_name = request.args.get('firstName')
+        l_name = request.args.get('lastName')
+        company = request.args.get('company')
+
+    match_field = {}
+    if f_name:
+        match_field['first_name'] = f_name
+    if l_name:
+        match_field['last_name'] = l_name
+    if company:
+        match_field['company.slug'] = slugify(company)
+
+    match = {
+        "$match": match_field
+    }
+
+    group = {
+        "$group": {
+            "_id": {
+                "_id": "$_id",
+                "first_name": "$first_name",
+                "last_name": "$last_name",
+                "job_title": "$job_title",
+                "company": {
+                    "name": "$company.slug",
+                    "name": "$company.name",
+                },
+                "phone": {
+                    "main_phone": "$phone.main_phone",
+                    "mobile": "$phone.mobile",
+                },
+                "email": "$email",
+            }
+        }
+    }
+
+    project = {
+        "$project": {
+            "_id": 0,
+            "_id": "$_id._id",
+            "first_name": "$_id.first_name",
+            "last_name": "$_id.last_name",
+            "job_title": "$_id.job_title",
+            "company": {
+                "name": "$_id.company.name",
+                "slug": "$_id.company.slug"
+            },
+            'phone': {
+                "main_phone": "$_id.phone.main_phone",
+                "mobile": "$_id.phone.mobile",
+            },
+            "email": "$_id.email",
+        }
+    }
+
+    pipeline = [match, group, project]
+
+    json_obj = mongo.db.customers.aggregate(pipeline)
+    print json_obj
     resp = Response(
         response=json_util.dumps(json_obj['result']),
         mimetype='application/json'
